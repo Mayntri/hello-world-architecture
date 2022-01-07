f1d25f4e703fbed904a1ae8a44805e64294012df
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-01-07 11:45:48 +0100

    typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+export * from './sum';
+
 export const logger = {
-    log: (...rest: any[]) => console.log(...rest),
-    info: (...rest: any[]) => console.info(...rest),
-    warn: (...rest: any[]) => console.warn(...rest),
-    error: (...rest: any[]) => console.error(...rest),
-}
+  /* eslint-disable no-console */
+  log: (...rest: unknown[]) => console.log(...rest),
+  info: (...rest: unknown[]) => console.info(...rest),
+  warn: (...rest: unknown[]) => console.warn(...rest),
+  error: (...rest: unknown[]) => console.error(...rest),
+  /* eslint-enable no-console */
+};
 
-export const ping = () => logger.log('pong')
+export const ping = () => logger.log('pong');
 
-export const time = () => new Date().toISOString()
+export const time = () => new Date().toISOString();
